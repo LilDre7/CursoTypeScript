f27e10b69aed7517241eec4cce66b28bf9ee9262
Alvaro Aburto <alvaroaburto71@gmail.com>
2023-09-07 16:44:35 -0600

    :memo: upadate document

--- EjerciciosConTypeScript/ejercicio#1.ts
+++ EjerciciosConTypeScript/ejercicio#1.ts
@@ -1,7 +1,16 @@
 // ** Encuentra los 2 primeros numeros del array y sumalos 🧨 //
 
-type NUM__ARR = (number | string)[] = []
+type NUM__ARR = (number | string[])[];
 
 const arrayNumbers: NUM__ARR = [
-    1, 3, 4, 5, 315, 35, 315, 3, 5, 35, 3, 2, 42, , 37, 73, 8, 988, 76,
-  ]
+  1, 3, 4, 5, 315, 35, 315, 3, 5, 35, 3, 2, 42, 37, 73, 8, 988, 76,
+];
+
+const sumTwoFirstNum = (arrayNumbers) => {
+  for (let i in arrayNumbers) {
+    return arrayNumbers[i]
+  }
+}
+
+console.log(sumTwoFirstNum(arrayNumbers))
+
