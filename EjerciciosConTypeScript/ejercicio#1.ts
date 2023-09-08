1ae374806be0ad79b4b7fe0b4cef06739555f7c0
Alvaro Aburto <alvaroaburto71@gmail.com>
2023-09-07 18:56:26 -0600

    :memo: upadate document

--- EjerciciosConTypeScript/ejercicio#1.ts
+++ EjerciciosConTypeScript/ejercicio#1.ts
@@ -2,18 +2,19 @@
 
 type NUM__ARR = (number | string[])[];
 
+// Definir un arreglo de números
 const arrayNumbers: NUM__ARR = [
   1, 3, 4, 5, 315, 35, 315, 3, 5, 35, 3, 2, 42, 37, 73, 8, 988, 76,
 ];
 
-const sumTwoFirstNum = (arrayNumbers) => {
-  for (let i = 0; i < arrayNumbers; i++) {
-    const element = arrayNumbers[i]
-    return element
+// Verificar que el arreglo tenga al menos dos elementos
+const sumArrNew = (arrayNumbers: any[]) => {
+  if (arrayNumbers.length < 2) {
+    return "Not Found"
+  } else {
+    const firstNum = arrayNumbers[0];
+    const secondNum = arrayNumbers[1];
+    return firstNum + secondNum;
   }
 }
-
-console.log(sumTwoFirstNum(arrayNumbers))
-
-  
-}
+console.log(sumArrNew(arrayNumbers))
